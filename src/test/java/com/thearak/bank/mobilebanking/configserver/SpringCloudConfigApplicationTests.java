74758ea88cd96869a281ed96fb74b44494f05841
sothearakseng <sothearakseng97@gmail.com>
2025-06-23 20:24:01 +0700

    Initial commit

---
+++ src/test/java/com/thearak/bank/mobilebanking/configserver/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thearak.bank.mobilebanking.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
